63806ecb152407824a3f65dfdf10d445671169d8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-20 22:04:48 -0400

    Make windowing object auto calculate properly

--- bin/windowing/window.dart
+++ bin/windowing/window.dart
@@ -1,4 +1,16 @@
 // Window handling class
+import 'package:raylib/raylib.dart';
+
+/*
+reminder on how to control window states
+if (isWindowState(ConfigFlags.windowResizable)) {
+  clearWindowState(ConfigFlags.windowResizable);
+} else {
+ setWindowState(ConfigFlags.windowResizable);
+}
+*/
+
+
 class Window {
 
   int sizeX = 0;
@@ -32,10 +44,41 @@ class Window {
     return aspect;
   }
 
-  String getTitle(){
+  String getTitle() {
     return title;
   }
 
+  // Regular polling during the update loop
+  void update() {
+    bool resized = isWindowResized();
+
+    if (resized) {
+      updateInternalSize();
+    }
+  }
+
+  // Internal calculator - Probably redundant
+  void updateInternalSize() {
+
+    bool fullScreen = isWindowState(ConfigFlags.fullscreenMode);
+
+    int currentMonitor = getCurrentMonitor();
+
+
+    if (fullScreen) {
+      int width = getMonitorWidth(currentMonitor);
+      int height = getMonitorHeight(currentMonitor);
+      resize(width, height);
+    } else {
+      int width = getScreenWidth();
+      int height = getScreenHeight();
+      resize(width, height);
+    }
+
+    debug();
+  }
+
+  // only used for debug
   void debug() {
     print("SizeX: $sizeX | SizeY: $sizeY | Aspect: $aspect");
   }
