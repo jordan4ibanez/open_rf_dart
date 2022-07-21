8309c5e1983e244dbbb1c1a24376cba76774dcd5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-20 20:01:31 -0400

    Draw a red window

--- bin/open_rf.dart
+++ bin/open_rf.dart
@@ -1,7 +1,13 @@
 import 'package:raylib/raylib.dart';
+
 import 'dart:io';
 
+import 'windowing/window.dart';
+
 void main() {
+
+  // Version number
+  const versionNumber = "0.0.0";
 
   // Get the current running directory of the game.
   final String currentPath = Directory.current.path;
@@ -12,7 +18,25 @@ void main() {
     linux: "$currentPath/libraylib.so",
     // Windows is on the todo list
     // windows: '../somewhere/else',
+    // Mac is currently unsupported
   );
 
-  
+  // Window object
+  Window window = Window(800, 400, "Open RF $versionNumber");
+
+  initWindow(window.getSizeX(), window.getSizeY(), window.getTitle());
+
+  setTargetFPS(60);
+
+  while(!windowShouldClose()) {
+    window.debug();
+
+
+    beginDrawing();
+
+    clearBackground(Color(255, 0, 0, 255));
+
+    endDrawing();
+  }
+
 }
