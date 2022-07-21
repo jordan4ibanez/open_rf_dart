9f965c7a1f1c0466ef536c2e62e798a1a6fa86bf
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-20 23:20:11 -0400

    Successfully render a textured rainbow trout

--- bin/open_rf.dart
+++ bin/open_rf.dart
@@ -39,9 +39,27 @@ void main() {
 
   setTargetFPS(60);
 
+  Model fish = loadModel("$currentPath/models/rainbowtrout.obj");
+  Texture fishTexture = loadTexture("$currentPath/models/rainbowtrout.png");
+  fish.materials[0].maps[materialMapDiffuse].texture = fishTexture;
+  
+
+
+  //fishMaterial.maps[materialMapDiffuse].texture = fishTexture;
+  
+
   int counter = 0;
 
+  final camera = Camera3D(
+    position: Vector3( 0.0, 10.0, 10.0 ),
+    target: Vector3.zero(),
+    up: Vector3( 0.0, 1.0, 0.0),
+    fovy: 45
+  );
+
   while(!windowShouldClose()) {
+
+    double delta = getFrameTime();
 
     // Event/engine calls
     window.update();
@@ -52,13 +70,47 @@ void main() {
     if (counter >= 180) {
       toggleFullscreen();
 
+      if (isWindowState(ConfigFlags.fullscreenMode)) {
+        setCameraMode(camera, CameraMode.orbital);
+        camera.target.x = 0;
+        camera.target.y = 0;
+        camera.target.z = 0;
+      } else {
+        setCameraMode(camera, CameraMode.firstPerson);
+      }
+
       counter = 0;
     }
 
 
 
     // Render calls
-    render.draw(random);
+    // render.draw(random);
+
+    updateCamera(camera);
+
+
+    // camera.target.y += delta * 10;
+
+    beginDrawing();
+
+    clearBackground(Color(255, 255, 255, 255));
+
+    beginMode3D(camera);
+
+    //drawCube(Vector3.zero(), 2, 2, 2, Color.red);
+
+    drawModel(fish, Vector3(2, 2, 2), 1, Color.rayWhite);
+
+
+    endMode3D();
+
+
+    // drawText("hi there", 0, 0, 24, Color.black);
+
+    
+    endDrawing();
+    
     
     // --- Repeat ----->
   }
